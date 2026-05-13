446cccf18a6fc6773b02d0a58fc8c3f05a806e55
Nguyen Thanh Nhan <mikmana01@gmail.com>
2026-05-13 11:07:47 +0700

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thông tin cá nhân</title>
+</head>
+<body>
+    <h2>THÔNG TIN CÁ NHÂN</h2>
+    <p>Họ tên: Nguyễn Thành Nhân</p>
+    <p>MSSV: 2124110299</p>
+    <p>Lớp: CCQ2411G</p>
+    <p>Khoa: CNTT</p>
+</body>
+</html>
