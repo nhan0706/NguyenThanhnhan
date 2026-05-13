831fa007442d8a22c940e60feb840f5e5c285d3b
Nguyen Thanh Nhan <mikmana01@gmail.com>
2026-05-13 12:26:12 +0700

    Thêm DemoController và các route/view lab

---
+++ resources/views/demoindex4.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo Index 4</title>
+</head>
+<body>
+    <h1>Demo Index 4</h1>
+    <p>Dữ liệu data: {{ $data }}</p>
+    <p>ID: {{ $id }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,10 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return "Test";
 });
+
+Route::get('/demo1', [DemoController::class, 'index']);
+Route::get('/demo2', [DemoController::class, 'index2']);
+Route::get('/demo3', [DemoController::class, 'index3']);
+Route::get('/demo4/{id}', [DemoController::class, 'index4']);
+Route::get('/demo5/{id?}', [DemoController::class, 'index5']);
+Route::get('/demo6/{param1}/{param2}', [DemoController::class, 'index6']);
